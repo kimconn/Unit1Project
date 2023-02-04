5a183a985d63a3e16796fd5b5bcd200400626ec7
KimC <kconn029@fiu.edu>
2023-02-04 11:10:39 -0500

    Project 1 - MyStory -- Holly

---
+++ Unit1Project/Detail View Controller.swift
@@ -0,0 +1,35 @@
+//
+//  Detail View Controller.swift
+//  Unit1Project
+//
+//  Created by Kim Connolly on 2/2/23.
+//
+
+import UIKit
+
+class Detail_View_Controller: UIViewController {
+
+    // Property
+//    var aboutMe: AboutMe?
+//    var likes: Likes?
+//    var jokes: Jokes?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Unit1Project/ViewController.swift
+++ Unit1Project/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var Holly: UIImageView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        Holly.layer.cornerRadius = Holly.frame.size.width / 2
+        Holly.clipsToBounds = true
         // Do any additional setup after loading the view.
+    }//viewdidload
+
+    @IBAction func didTapAboutMe(_ sender: UITapGestureRecognizer) {
+        
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "aboutMe", sender: tappedView)
+        }
     }
 
+    
+    @IBAction func didTapLikes(_ sender: UITapGestureRecognizer) {
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "likes", sender: tappedView)
+        }
+    }
+    
+    
 
+    @IBAction func didTapJokes(_ sender: UITapGestureRecognizer) {
+    
+    if let tappedView = sender.view {
+        performSegue(withIdentifier: "jokes", sender: tappedView)
+    }
 }
 
+    
+}//viewdidload
